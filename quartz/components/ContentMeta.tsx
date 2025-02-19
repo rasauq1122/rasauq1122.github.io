a0a7a07b48585e668811a32de4bbebbd68a0d10e
Junseong <hell-o-world@naver.com>
2025-02-19 20:43:09 +0900

    feat: ContentMeta도 obsidian의 created 프론트매터를 참고하도록 변경

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -1,4 +1,4 @@
-import { Date, getDate } from "./Date"
+import { formatDate, getDate } from "./Date"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import readingTime from "reading-time"
 import { classNames } from "../util/lang"
@@ -29,9 +29,8 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
     if (text) {
       const segments: (string | JSX.Element)[] = []
 
-      if (fileData.dates) {
-        segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
-      }
+      const fileCreated = new Date(String(fileData.frontmatter?.created))
+      segments.push(formatDate(fileCreated, cfg.locale))
 
       // Display reading time if enabled
       if (options.showReadingTime) {
