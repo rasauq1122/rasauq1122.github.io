b02300a0bf6bde4180360427858511f759f74f6d
Junseong <hell-o-world@naver.com>
2025-02-19 16:27:23 +0900

    feat: 블로그 title 변경

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "배워서 남주자",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
