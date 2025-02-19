f93cbb4d30faf71e62669162bbf4d07fdbb0b5e7
Junseong <hell-o-world@naver.com>
2025-02-19 21:02:16 +0900

    feat: 추가한 scheme 내용을 전달해줄 수 있도록 변경

--- quartz/util/theme.ts
+++ quartz/util/theme.ts
@@ -53,6 +53,8 @@ ${stylesheet.join("\n\n")}
   --tertiary: ${theme.colors.lightMode.tertiary};
   --highlight: ${theme.colors.lightMode.highlight};
   --textHighlight: ${theme.colors.lightMode.textHighlight};
+  --documentBackground: ${theme.colors.lightMode.documentBackground};
+  --documentShadow: ${theme.colors.lightMode.documentShadow};
 
   --headerFont: "${theme.typography.header}", ${DEFAULT_SANS_SERIF};
   --bodyFont: "${theme.typography.body}", ${DEFAULT_SANS_SERIF};
@@ -69,6 +71,8 @@ ${stylesheet.join("\n\n")}
   --tertiary: ${theme.colors.darkMode.tertiary};
   --highlight: ${theme.colors.darkMode.highlight};
   --textHighlight: ${theme.colors.darkMode.textHighlight};
+  --documentBackground: ${theme.colors.lightMode.documentBackground};
+  --documentShadow: ${theme.colors.lightMode.documentShadow};
 }
 `
 }
